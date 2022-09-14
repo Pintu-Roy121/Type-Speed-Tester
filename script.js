c63e74248a64181077498dfa5ceda5ed9594ab60
Pintu-Roy121 <roypintu121@gmail.com>
2022-09-15 01:28:08 +0600

    Mistakes count solved

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ const typeController = (e) => {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
     errorCount++;
   }
-
   // check if given question text is equal to user typed text
   if (questionText === userText) {
     gameOver();
